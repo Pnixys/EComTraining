51246f41b3a881cc3aa440abb05791b6709a0880
Julien <julien.daugaronthe@gmail.com>
2023-01-30 21:43:18 +0100

    Modify Layout.jsx to Layout.tsx, put correct type on context. Change the height of the div that contain all the element to fit all the screen

---
+++ e-com-training/src/layout/Layout.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useState } from 'react'
+
+import { logo } from '../assets'
+import Footer from './components/Footer'
+
+import ReactSwitch from 'react-switch'
+
+export const ThemeContext = createContext({});
+
+const Layout = ({children}:any) => {
+    
+    const [theme, setTheme] = useState("theme-light");
+
+    const toggleTheme = () => {
+        setTheme((currentTheme) => currentTheme === "theme-light" ? "theme-dark" : "theme-light")
+    }
+
+    return (
+            <ThemeContext.Provider value={{ theme, toggleTheme }}>
+                <div id={theme}>
+                    <div className="grid grid-cols-3 content-center h-16">
+                        <img
+                            src={logo}
+                            alt='logo of the website'
+                            className="object-scale-down h-14 w-16"/>
+                        <h1 className='place-self-center p-2'>
+                            Nom du site
+                        </h1>
+                        <ReactSwitch
+                                    className='justify-self-end mr-10'
+                                    onChange={toggleTheme} 
+                                    checked={theme === "theme-dark"}
+                                    onColor='#fc850d'
+                                    offColor='#6f36ba'/>
+                    </div>
+                    {children}
+                    <Footer />
+                </div>
+            </ThemeContext.Provider>
+    )
+}
+
+export default Layout
--- e-com-training/src/layout/components/Footer.tsx
+++ e-com-training/src/layout/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div></div>
+        <div className="absolute inset-x-0 bottom-0 h-16">FOOTER</div>
     )
 }
 
