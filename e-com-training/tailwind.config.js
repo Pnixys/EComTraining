bcc60156e4b34499f4c6289f929365a8c3187592
Julien <50711686+Pnixys@users.noreply.github.com>
2023-02-07 21:11:53 +0100

    workflow again (#6)

--- e-com-training/tailwind.config.js
+++ e-com-training/tailwind.config.js
@@ -2,13 +2,11 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{html,js}',
-    './node_modules/tw-elements/dist/js/**/*.js'
+    './src/**/*.{html,js}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('tw-elements/dist/plugin')
   ],
 }
